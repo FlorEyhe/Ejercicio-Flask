4486c23053f267b8a77f488a3ede53094c61260e
Florencia <florenciaeyherabide@gmail.com>
2023-05-02 23:47:07 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify, request
+import os
+import pickle
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "API para prediccion"
+
+@app.route('/api/test', methods=['GET'])
+def test():
+    return "API TEST"
+
+
+@app.route('/api/v1/predict', methods=['GET'])
+def prediction():
+    
+    tv = int(request.args['TV'])
+    radio = int(request.args['radio'])
+    newspaper = int(request.args['newspaper'])
+
+    #Cargamos el modelo
+
+    loaded_model = pickle.load(open('./model.pkl', 'rb'))
+
+    new_data = [tv, radio, newspaper]
+
+    #Realizamos la preduccion
+    prediction_model = loaded_model.predict([new_data])
+    
+
+    #Devolvemos la prediccion
+
+    return jsonify({"repository":prediction_model[0]})
+    
+if __name__ == '__main__':
+    app.run(debug = True, port=5000)
